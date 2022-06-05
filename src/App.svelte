7fbca6091bc630caf6dc003ac9950ca51c45a287
Akash <bhosaleakash3915@gmail.com>
2022-06-06 01:15:29 +0530

    Svelte api data fetch

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount } from "svelte";
+
+  let res = [];
+  onMount(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    res = await response.json();
+  });
+</script>
+
+<main>
+  {#each res as post}
+    <div class="post">
+      <h3>{post.title}</h3>
+      <h5>{post.body}</h5>
+    </div>
+  {/each}
+</main>
+
+<style>
+  .post {
+    width: 80%;
+    margin: 15px auto;
+    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    padding: 10px;
+  }
+
+  .post h3 {
+    margin-bottom: 10px;
+    text-transform: capitalize;
+  }
+</style>
